0cafebb6cba764ebd8b32f31d5974b9798547ab4
Jay <the7mincheol@gmail.com>
2016-08-16 12:06:11 +0900

    pyqt4 added

---
+++ PyQt4/hufsgrade.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
+
+form_class = uic.loadUiType("main_window.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
+
+# 진도체크: https://wikidocs.net/5227
